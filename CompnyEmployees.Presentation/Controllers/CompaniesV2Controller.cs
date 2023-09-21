73ff560d6ac65f324955853796259572d284729b
TyGraced <temitayoadesugba@gmail.com>
2023-09-21 16:14:42 +0100

    Versioning

---
+++ CompnyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -0,0 +1,32 @@
+﻿using CompnyEmployees.Presentation.ActionFilters;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.RequestFeatures;
+using System.Text.Json;
+
+namespace CompnyEmployees.Presentation.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesV2Controller : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public CompaniesV2Controller(IServiceManager service) => _service = service;
+
+        [HttpGet("Version")]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public async Task<IActionResult> GetCompanies([FromQuery] CompanyParameters companyParameters)
+        {
+            var linkParams = new LinkCompanyParmeters(companyParameters, HttpContext);
+            var result = await _service.CompanyService
+                    .GetAllCompaniesAsync(linkParams, trackChanges: false);
+
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(result.metaData));
+
+            return result.linkResponse.HasLinks ? Ok(result.linkResponse.LinkedEntities) :
+                Ok(result.linkResponse.ShapedEntities);
+        }
+    }
+}
